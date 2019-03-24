03db1181056893f3ff46c4ff83808f804ba77f16
kavinkuma6 <kavinkumar1543884@gmail.com>
2019-03-25 01:41:19 +0530

    Database creation program in python

---
+++ Raspberry-Pi/create_DB.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+# SQLite DB Name
+DB_Name =  "agristick.db"
+
+# SQLite DB Table Schema for sensor data raw value
+TableSchema_SD="""
+drop table if exists sendata ;
+create table sendata (
+  id integer primary key autoincrement,
+  date_time text,
+  soil_temp float,
+  soil_moist int,
+  atmp_temp float,
+  atmp_hum float
+);
+
+"""
+
+
+#Connect or Create DB File
+conn = sqlite3.connect(DB_Name)
+curs = conn.cursor()
+
+#Create Tables for sensor data raw value
+sqlite3.complete_statement(TableSchema_SD)
+curs.executescript(TableSchema_SD)
+
+
+#Close DB
+curs.close()
+conn.close()
+
+
+
